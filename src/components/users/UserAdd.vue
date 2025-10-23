42a6d74ecdec7de1bd0357206c55e4fd3bf06c3a
deskcodeFun <panya.korkusol@gmail.com>
2025-10-23 08:16:52 +0700

    modified:   src/components/users/UserAdd.vue   create form and function addUser

--- src/components/users/UserAdd.vue
+++ src/components/users/UserAdd.vue
@@ -1,7 +1,65 @@
 <template>
-  <div>
-    <h1>Add New User</h1>
-  </div>
+  <header class="flex flex-row justify-between p-4 sm:pl-8 py-2">
+    <p class="font-semibold text-xl text-sky-900 text-shadow-lg">Add New User</p>
+    <button
+      @click="router.back()"
+      class="flex flex-row mt-1 ml-4 px-2 rounded-sm border-1 border-blue-800 text-blue-900 font-semibold bg-white/25 hover:bg-blue-900 hover:text-white"
+    >
+      <ChevronDoubleLeftIcon class="h-6 w-6 mr-1 text-gray-500" />
+
+      Back
+    </button>
+  </header>
+
+  <main class="flex flex-col p-2 sm:pl-8">
+    <form class="w-auto sm:max-w-screen-sm flex flex-col gap-4" @submit.prevent="addSubmit">
+      <label for="fname">First Name</label>
+      <input type="text" v-model="fname" class="bg-sky-50" />
+      <label for="lname">Last Name</label>
+      <input type="text" v-model="lname" class="bg-sky-50" />
+      <label for="email">Email</label>
+      <input type="text" v-model="email" class="bg-sky-50" />
+      <label for="department">Department</label>
+      <input type="text" v-model="department" class="bg-sky-50" />
+
+      <label for="offie_id">Office Name</label>
+      <input type="number" v-model="office_id" class="bg-sky-50" />
+    </form>
+    <button
+      class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-8 mt-4 rounded-full w-full sm:w-fit focus:outline-hidden focus:shadow-outline"
+      type="submit"
+      @click="addSubmit"
+    >
+      Save
+    </button>
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { ChevronDoubleLeftIcon } from '@heroicons/vue/20/solid'
+import { mockStaff } from '@/dataMockup/staff'
+import { useRouter } from 'vue-router'
+import { ref, reactive } from 'vue'
+
+const router = useRouter()
+
+const staff = reactive(mockStaff)
+const fname = ref('')
+const lname = ref('')
+const email = ref('')
+const department = ref('')
+const office_id = ref('')
+const newUser = reactive({
+  fname: fname,
+  lname: lname,
+  email: email,
+  department: department,
+  office_id: office_id,
+})
+const addSubmit = () => {
+  // console.log(newUser)
+  staff.push(newUser)
+  // console.log('After add user: ', staff)
+  router.back()
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,13 @@
 <template>
-  <button @click="addUser">Add user</button>
   <!-- <div><h2 class="text-2xl text-blue-900">HomeView</h2></div>
   <button @click="showSuccess">Show Success Toast</button> -->
-  <div class="flex flex-row flex-wrap justify-center sm:justify-start sm:ml-8">
-    <UserCard v-for="(staff, index) in staffs" :key="index" :staff="staff" />
-  </div>
+  <div></div>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
-import { mockStaff } from '@/dataMockup/staff'
-import UserCard from '@/components/users/UserCard.vue'
-import router from '@/router'
 // import { useToast } from 'vue-toastification'
 
 // const toast = useToast()
-
-const staffs = reactive(mockStaff)
-const addUser = () => {
-  router.push('/addUser')
-}
-
 // const showSuccess = () => {
 //   toast.success('Testing success Tostification')
 // }
