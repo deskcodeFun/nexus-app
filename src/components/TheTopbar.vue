f8470a22dcab08f3354978adcd263e2f20de3278
deskcodeFun <panya.korkusol@gmail.com>
2025-11-17 09:16:52 +0700

    modified:   src/components/TheTopbar.vue fix: move close icon into navbar, modified UI

--- src/components/TheTopbar.vue
+++ src/components/TheTopbar.vue
@@ -7,25 +7,31 @@ const navOpen = ref(false)
 </script>
 
 <template>
-  <div class="pt-2 pb-4 px-2 rounded-md">
+  <div class="py-2 px-2 rounded-md">
     <div class="flex flex-row">
       <button @click="navOpen = !navOpen">
         <Bars3Icon
           :class="navOpen ? 'hidden' : ''"
           class="h-6 w-6 mt-1 rounded-full bg-gray-50/10 text-blue-900"
         />
-        <XMarkIcon
+        <!-- <XMarkIcon
           :class="navOpen ? '' : 'hidden'"
           class="h-6 w-6 rounded-full bg-gray-50/25 text-red-900"
-        />
+        /> -->
       </button>
       <p class="ml-2 font-bold text-2xl text-sky-900 text-shadow-lg">Nexus Service</p>
     </div>
   </div>
   <Transition name="slide-fade">
     <Teleport to="body">
-      <div v-if="navOpen" class="modal mt-14 sm:ml-8" @click.self="navOpen = false">
-        <nav class="flex flex-col py-4 w-64 bg-sky-50">
+      <div v-if="navOpen" class="modal mt-16" @click.self="navOpen = false">
+        <nav class="flex flex-col py-2 w-64 bg-sky-50">
+          <button @click="navOpen = !navOpen" class="flex justify-end px-2">
+            <XMarkIcon
+              :class="navOpen ? '' : 'hidden'"
+              class="h-6 w-6 rounded-full bg-gray-50/25 text-red-900 hover:bg-red-900 hover:text-white"
+            />
+          </button>
           <RouterLink
             to="/"
             class="px-8 text-lg rounded-xl m-1 p-1 hover:bg-gray-200 md:text-lg"
