b823f306b2cdfdf39a8798d88de9b25a86062fd5
deskcodeFun <panya.korkusol@gmail.com>
2025-11-28 11:54:54 +0700

    modified:   src/views/AssetsView.vue   fix ui

--- src/views/AssetsView.vue
+++ src/views/AssetsView.vue
@@ -2,10 +2,10 @@
   <main class="bg-white">
     <!-- submenu filter dropdown select BU-->
     <div class="flex flex-col justify-between bg-white sm:justify-normal px-2 py-2">
-      <p class="text-lg mt-1 mb-2 pb-4 text-blue-900">Assets Account</p>
-      <div class="flex flex-row gap-4">
+      <p class="text-lg mt-1 mb-4 text-blue-900">Assets Account</p>
+      <div class="flex flex-row mb-4 gap-4">
         <p>Select BU</p>
-        <select class="text-sm border-b">
+        <select class="text-sm bg-gray-50">
           <!-- <option value="" disabled>Select BU</option> -->
           <option value="0">All</option>
           <option
@@ -20,10 +20,10 @@
     </div>
 
     <!--Show computer  -->
-    <div class="flex flex-row py-4 px-4 gap-18 hover:cursor-pointer">
-      <p @click="toggleCom" class="hover:border-b px-4">Computer</p>
-      <p @click="togglePrinter" class="hover:border-b px-4">Printer</p>
-      <p @click="toggleOther" class="hover:border-b px-4">Other</p>
+    <div class="flex flex-row px-2 gap-4 hover:cursor-pointer">
+      <p @click="toggleCom" class="hover:border-b">Computer</p>
+      <p @click="togglePrinter" class="hover:border-b">Printer</p>
+      <p @click="toggleOther" class="hover:border-b">Other</p>
     </div>
     <Transition name="slide-down">
       <ComputerView v-if="showCom"></ComputerView>
@@ -34,26 +34,6 @@
     <Transition name="slide-down">
       <PrinterView v-if="showOther"></PrinterView>
     </Transition>
-
-    <!-- <section class="py-4 px-4">
-      <p>Computer</p>
-      <ComputerView />
-      <p>Printer</p>
-      <PrinterView />
-      <p>Other</p>
-      <PrinterView />
-    </section> -->
-    <!-- <section>
-      <BaseAccordion title="Computer">
-        <ComputerView />
-      </BaseAccordion>
-      <BaseAccordion title="Printer">
-        <PrinterView />
-      </BaseAccordion>
-      <BaseAccordion title="Other">
-        <PrinterView />
-      </BaseAccordion>
-    </section> -->
   </main>
 </template>
 
