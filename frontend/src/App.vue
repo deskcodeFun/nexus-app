cd86c159d87ac0cc6ebda4c7cd4cf79b87af9f91
deskcodeFun <panya.korkusol@gmail.com>
2025-12-01 08:04:28 +0700

    modified:   frontend/src/App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <!-- <div class="w-full min-h-screen bg-linear-180 from-indigo-100 to-red-100"> -->
   <div class="w-full min-h-screen">
-    <TheTopbar />
-    <RouterView />
+    <header>
+      <TheTopbar />
+    </header>
+    <main class=" sm:px-8">
+      <RouterView />
+    </main>
   </div>
 </template>
 
