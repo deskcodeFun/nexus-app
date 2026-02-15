4da2783938d87d3e62bcfc73d00eb296aa7ef866
deskcodeFun <panya.korkusol@gmail.com>
2026-02-15 11:06:09 +0700

    modified: EmployeeCard.vue

--- frontend/src/components/employees/EmployeeCard.vue
+++ frontend/src/components/employees/EmployeeCard.vue
@@ -1,40 +1,43 @@
 <template>
-  <div v-for="employee in store.employee" :key="employee.id" @click="router.push(`/editEmployee/${employee.id}`)"
-    class="flex justify-center mx-2">
-    <BaseCard>
-      <!-- show data -->
-      <table
-        class="[&_th]:pt-1 [&_th]:text-left [&_th]:align-baseline [&_th]:font-light [&_th]:text-xs [&_th]:text-gray-500 [&_td]:text-blue-900 [&_td]:pl-2">
-        <tbody>
-          <tr>
-            <th>User ID</th>
-            <!-- <td>{{ currentUser.fname + ' ' + currentUser.lname }}</td> -->
-            <td>
-              {{ employee.id }}
-            </td>
-          </tr>
-          <tr>
-            <th>User Name</th>
-            <td>
-              {{ employee.fname + ' ' + employee.lname.substring(0, 1) + '.' }}
-            </td>
-          </tr>
+  <div class="flex justify-center flex-row flex-wrap ">
 
-          <tr>
-            <th scope="col">Email</th>
-            <td>{{ employee.email }}</td>
-          </tr>
-          <tr>
-            <th scope="col">Department</th>
-            <td>{{ employee.department }}</td>
-          </tr>
-          <tr>
-            <th scope="col">Office</th>
-            <td>{{ employee.office_name.name }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </BaseCard>
+    <div v-for="employee in store.employee" :key="employee.id" @click="router.push(`/editEmployee/${employee.id}`)"
+      class="flex justify-center mx-2">
+      <BaseCard>
+        <!-- show data -->
+        <table
+          class="[&_th]:pt-1 [&_th]:text-left [&_th]:align-baseline [&_th]:font-light [&_th]:text-xs [&_th]:text-gray-500 [&_td]:text-blue-900 [&_td]:pl-2">
+          <tbody>
+            <tr>
+              <th>User ID</th>
+              <!-- <td>{{ currentUser.fname + ' ' + currentUser.lname }}</td> -->
+              <td>
+                {{ employee.id }}
+              </td>
+            </tr>
+            <tr>
+              <th>User Name</th>
+              <td>
+                {{ employee.fname + ' ' + employee.lname.substring(0, 1) + '.' }}
+              </td>
+            </tr>
+
+            <tr>
+              <th scope="col">Email</th>
+              <td>{{ employee.email }}</td>
+            </tr>
+            <tr>
+              <th scope="col">Department</th>
+              <td>{{ employee.department }}</td>
+            </tr>
+            <tr>
+              <th scope="col">Office</th>
+              <td>{{ employee.office_name.name }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </BaseCard>
+    </div>
   </div>
 </template>
 
