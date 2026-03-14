5f13ac2e02dd1fd70288012245e97d630315a910
deskcodeFun <panya.korkusol@gmail.com>
2026-03-14 07:40:11 +0700

    modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/nexus-app/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    base: 'https://deskcodefun.github.io/nexus-app/',
   },
 })
