4c53bbed84d1ac2245b81cf612e71890ed09d6f7
deskcodeFun <panya.korkusol@gmail.com>
2026-03-14 09:51:40 +0700

    modified:   src/views/HomeView.vue modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicPath: 'https://deskcodefun.github.io/gh-pages/',
+  publicPath: 'https://deskcodefun.github.io/nexus-app/gh-pages/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
