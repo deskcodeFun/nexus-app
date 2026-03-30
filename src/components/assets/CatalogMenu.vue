70e12f98747a5f63168b275340df80e6c2462643
deskcodeFun <panya.korkusol@gmail.com>
2026-03-30 09:37:21 +0700

    fix UI: CatalogMenu.vue   on state active, focud   bg-blue-900 and text-white

--- src/components/assets/CatalogMenu.vue
+++ src/components/assets/CatalogMenu.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="submenu">
-    <button class="px-2 mx-2 rounded-md bg-sky-100 text-blue-900 hover:bg-blue-900 hover:text-white"
+    <button
+      class="px-2 mx-2 rounded-md bg-sky-100 text-blue-900 hover:bg-blue-900 hover:text-white active:bg-blue-900 focus:bg-blue-900 focus:text-white"
       @click="$emit('changeView', 'ComputerAdd')">
       Computer
     </button>
-    <button class="px-2 mx-2 rounded-md bg-sky-100 text-blue-900 hover:bg-blue-900 hover:text-white"
+    <button
+      class="px-2 mx-2 rounded-md bg-sky-100 text-blue-900 hover:bg-blue-900 hover:text-white active:bg-blue-900 focus:bg-blue-900 focus:text-white"
       @click="$emit('changeView', 'PrinterAdd')">
       Printer
     </button>
-    <button class="px-2 mx-2 rounded-md bg-sky-100 text-blue-900 hover:bg-blue-900 hover:text-white"
+    <button
+      class="px-2 mx-2 rounded-md bg-sky-100 text-blue-900 hover:bg-blue-900 hover:text-white active:bg-blue-900 focus:bg-blue-900 focus:text-white"
       @click="$emit('changeView', 'OtherAdd')">
       Other
     </button>
