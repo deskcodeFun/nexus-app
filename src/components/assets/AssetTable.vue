41bffd82e201882ca9f95bd34b49a8806ce2e666
deskcodeFun <panya.korkusol@gmail.com>
2026-04-05 10:09:13 +0700

    new file:   src/components/BaseUploadImage.vue 	modified:   src/components/assets/AssetTable.vue 	modified:   src/components/employees/EmployeeTable.vue update UI: AsssetTable, EmployeeTable  fix overflow-y-scroll show under header Table  by separate <table> for <theaer> and <tbody>

--- src/components/assets/AssetTable.vue
+++ src/components/assets/AssetTable.vue
@@ -1,42 +1,46 @@
 <template>
   <!-- show data h-[430px] -->
-  <div class=" max-h-screen flex m-2 overflow-y-scroll overflow-x-hidden">
-    <table
-      class="table-auto h-fit w-full [&_th]:p-4 [&_th]:text-left [&_th]:align-baseline  [&_td]:text-blue-900 [&_td]:py-4 [&_td]:px-2 [&_td]:border-b [&_td]:border-slate-300">
-      <!-- Table Header -->
-      <thead class="table-header-group sticky top-0">
-        <tr class=" border-b border-gray-300  bg-sky-100 text-nowrap">
-          <th scope="col" class="text-left text-base p-2 font-bold text-blue-900">Asset Tag</th>
-          <th scope="col" class="text-left text-base p-2 font-bold text-blue-900">Serial</th>
-          <th scope="col" class="text-left text-base p-2 font-bold text-blue-900">Brand</th>
-          <th scope="col" class="text-left text-base p-2 font-bold text-blue-900">Model</th>
-          <th scope="col" class="text-left text-base p-2 font-bold text-blue-900">Used By</th>
-          <th scope="col" class=" text-left text-base p-2 font-bold text-blue-900">
+  <div class="max-h-screen m-2 overflow-clip  rounded-lg">
+    <!-- Table Header -->
+    <table class="table-fixed w-full [&_th]:p-4 [&_th]:text-left [&_th]:align-baseline  ">
+      <thead class=" sticky top-0 overflow-y-hidden bg-sky-100  z-10">
+        <tr class="">
+          <th class="text-left text-base p-2 font-bold text-blue-900">Asset Tag</th>
+          <th class="text-left text-base p-2 font-bold text-blue-900">Serial</th>
+          <th class="text-left text-base p-2 font-bold text-blue-900">Brand</th>
+          <th class="text-left text-base p-2 font-bold text-blue-900">Model</th>
+          <th class="text-left text-base p-2 font-bold text-blue-900">Used By</th>
+          <th class="text-left text-base p-2 font-bold text-blue-900">
             Asset BU</th>
         </tr>
       </thead>
-      <!-- Table body data -->
-      <tbody class="text-blue-900 overflow-hidden">
-        <tr v-for="item in store.asset" :key="item.id" @click="router.push(`/editAsset/${item.id}`)"
-          class="table-row  hover:shadow-2xl hover:bg-blue-50  hover:cursor-pointer text-nowrap">
-          <!-- <td class=" flex justify-center">{{ item.id }}</td> -->
-          <td class=" table-cell font-bold">{{ item.asset_tag }}</td>
-          <td class="table-cell">{{ item.serial_tag }}</td>
-          <td class="table-cell">{{ item.brand }}</td>
-          <td class="table-cell">{{ item.model }}</td>
-
-          <!-- User Full name -->
-          <td class="table-cell ">
-            <p v-if="item.employee">
-              {{ item.employee.fname + ' ' + item.employee.lname }}
-            </p>
-            <p v-else class="font-bold text-green-800">Free</p>
-          </td>
-          <td class="  ">{{ item.office_name.name }}</td>
-          <!-- <td>{{ item.catalog }}</td> -->
-        </tr>
-      </tbody>
     </table>
+    <!-- Table body data -->
+    <div class="flex-1 max-h-screen overflow-y-scroll">
+      <table
+        class="w-full table-fixed [&_td]:text-blue-900 [&_td]:py-4 [&_td]:px-2 [&_td]:border-b [&_td]:border-slate-300">
+        <tbody class="text-blue-900">
+          <tr v-for="item in store.asset" :key="item.id" @click="router.push(`/editAsset/${item.id}`)"
+            class="table-row  hover:shadow-2xl hover:bg-blue-50  hover:cursor-pointer text-nowrap">
+            <!-- <td class=" flex justify-center">{{ item.id }}</td> -->
+            <td class=" table-cell font-bold">{{ item.asset_tag }}</td>
+            <td class="table-cell">{{ item.serial_tag }}</td>
+            <td class="table-cell">{{ item.brand }}</td>
+            <td class="table-cell">{{ item.model }}</td>
+
+            <!-- User Full name -->
+            <td class="table-cell ">
+              <p v-if="item.employee">
+                {{ item.employee.fname + ' ' + item.employee.lname }}
+              </p>
+              <p v-else class="font-bold text-green-800">Free</p>
+            </td>
+            <td class="  ">{{ item.office_name.name }}</td>
+            <!-- <td>{{ item.catalog }}</td> -->
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
--- src/components/employees/EmployeeTable.vue
+++ src/components/employees/EmployeeTable.vue
@@ -1,27 +1,32 @@
 <template>
-  <div class="max-h-screen flex m-2 overflow-y-scroll overflow-x-hidden">
-    <table
-      class="table-auto h-fit w-full [&_th]:p-4 [&_th]:text-left [&_th]:align-baseline [&_td]:text-blue-900 [&_td]:py-4 [&_td]:px-2 [&_td]:border-b [&_td]:border-slate-300">
+  <div class="max-h-screen  m-2 overflow-clip">
+    <!-- Table Header -->
+    <table class="table-fixed w-full [&_th]:p-4 [&_th]:text-left [&_th]:align-baseline ">
       <thead class="table-header-group sticky top-0">
         <tr class="border-b border-gray-300 bg-sky-100 text-nowrap">
-          <th scope="col" class=" text-left text-base p-2 font-bold text-blue-900">User Name</th>
-          <th scope="col" class=" text-left text-base p-2 font-bold text-blue-900">Email</th>
-          <th scope="col" class=" text-left text-base p-2 font-bold text-blue-900">Department</th>
-          <th scope="col" class=" text-left text-base p-2 font-bold text-blue-900">Office</th>
+          <th scope="col" class="text-left text-base p-2 font-bold text-blue-900">User Name</th>
+          <th scope="col" class="text-left text-base p-2 font-bold text-blue-900">Email</th>
+          <th scope="col" class="text-left text-base p-2 font-bold text-blue-900">Department</th>
+          <th scope="col" class="text-left text-base p-2 font-bold text-blue-900">Office</th>
         </tr>
       </thead>
-
-      <tbody class="p-4 text-blue-900 overflow-hidden">
-        <tr v-for="employee in store.employee" :key="employee.id" @click="router.push(`/editEmployee/${employee.id}`)"
-          class="table-row hover:shadow-2xl hover:bg-blue-50  hover:cursor-pointer text-nowrap">
-          <td class="table-cell font-bold">{{ employee.fname + ' ' + employee.lname }}</td>
-          <td class="table-cell">{{ employee.email }}</td>
-          <td class="table-cell">{{ employee.department }}</td>
-          <!-- <td class="m-2 p-2">{{ employee.office_id}}</td> -->
-          <td class="table-cell m-2 p-2">{{ employee.office_name.name }}</td>
-        </tr>
-      </tbody>
     </table>
+    <!-- Table body data -->
+    <div class="flex-1 max-h-screen overflow-y-scroll">
+      <table
+        class="w-full table-fixed [&_td]:text-blue-900 [&_td]:py-4 [&_td]:px-2 [&_td]:border-b [&_td]:border-slate-300">
+        <tbody class="text-blue-900">
+          <tr v-for="employee in store.employee" :key="employee.id" @click="router.push(`/editEmployee/${employee.id}`)"
+            class="table-row hover:shadow-2xl hover:bg-blue-50 hover:cursor-pointer text-nowrap">
+            <td class="table-cell font-bold">{{ employee.fname + ' ' + employee.lname }}</td>
+            <td class="table-cell">{{ employee.email }}</td>
+            <td class="table-cell">{{ employee.department }}</td>
+            <!-- <td class="m-2 p-2">{{ employee.office_id}}</td> -->
+            <td class="table-cell m-2 p-2">{{ employee.office_name.name }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
