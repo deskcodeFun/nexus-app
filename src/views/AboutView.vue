7c661e12b778a29fac8740cb16112eef11315043
deskcodeFun <panya.korkusol@gmail.com>
2026-04-17 11:19:47 +0700

    AboutView.vue add and show multi image file with local URL

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,50 +1,59 @@
 <template>
-  <div class="p-24 bg-amber-50">
-
-    <input type="file" @change="uploadImage" accept="image/*" />
+  <div class="mt-28">
+    <label for="file-input" class="border px-4 py-2 bg-amber-50">Upload File</label>
+    <input type="file" id="file-input" accept="image/*" @change="handleFileSelect" multiple hidden />
   </div>
 
-  <div class="bg-sky-100 p-12">
-    <div v-for="image in images" :key="image.name">
-      <div v-if="image.name !== '.emptyFolderPlaceholder'">
-        <p>{{ image.name }}</p>
-        <img :src="image.url" :alt="image.name" style="width: 200px; margin: 10px;" />
+  <div class="bg-sky-100 p-12 mt-4 flex flex-row ">
+    <div class="flex flex-row gap-12">
+      <div v-for="(url, index) in previews" :key="index" class="flex flex-col items-center gap-3">
+        <img :src="url" alt="Selected image preview" class="w-25 h-25 object-cover" />
+        <p class="flex ">{{ files[index].name }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  import { supabase } from '@/lib/supabaseClient'
-  import { onMounted, ref } from 'vue'
-  const images = ref([])
+  import { ref } from 'vue'
+  const files = ref([])
+  const previews = ref([])
 
-  const fetchImages = async () => {
-    // 1. List all files in the bucket
-    const { data, error } = await supabase.storage.from('test').list()
-    console.log('fetch images error: ', error)
-    if (data) {
-      // 2. Generate public URLs for each file
-      images.value = data.map((file) => {
-        const { data: { publicUrl } } = supabase.storage
-          .from('test')
-          .getPublicUrl(file.name)
-        return { name: file.name, url: publicUrl }
-      })
-    }
+  function handleFileSelect(event) {
+    const input = event.target
+    const filesAsArray = Array.from(input?.files || [])
+    files.value = files.value.concat(filesAsArray)
+    console.log(' filesAsArray: ', filesAsArray)
+    console.log(' files: ', files)
+    const newPreviews = filesAsArray.map((file) => URL.createObjectURL(file))
+    previews.value = previews.value.concat(newPreviews)
   }
+  // const fetchImages = async () => {
+  //   // 1. List all files in the bucket
+  //   const { data, error } = await supabase.storage.from('test').list()
+  //   console.log('fetch images error: ', error)
+  //   if (data) {
+  //     // 2. Generate public URLs for each file
+  //     images.value = data.map((file) => {
+  //       const {
+  //         data: { publicUrl },
+  //       } = supabase.storage.from('test').getPublicUrl(file.name)
+  //       return { name: file.name, url: publicUrl }
+  //     })
+  //   }
+  // }
 
-  const uploadImage = async (event) => {
-    const file = event.target.files[0]
-    const fileName = `${file.name}`
+  // const uploadImage = async (event) => {
+  //   const file = event.target.files[0]
+  //   const fileName = `${file.name}`
 
-    const { data, error } = await supabase.storage
-      .from('test')
-      .upload(fileName, file)
-    console.log('data to uploadImage', data)
-    if (error) console.error('Upload error:', error.message)
-    else fetchImages() // Refresh test after upload
-  }
+  //   const { data, error } = await supabase.storage
+  //     .from('test')
+  //     .upload(fileName, file)
+  //   console.log('data to uploadImage', data)
+  //   if (error) console.error('Upload error:', error.message)
+  //   else fetchImages() // Refresh test after upload
+  // }
 
   // function uploadImage(e) {
   //   const file = e.target.files[0]
@@ -68,13 +77,15 @@
   //   }
   // }
 
+  // filesAsArray.forEach((file) => {
+  //   const reader = new FileReader()
+  //   reader.onload = (event) => {
+  //     const content = event.target.result
+  //     console.log('contensts of file: ', content)
+  //   }
+  //   reader.onerror = (error) => console.error('Error reading file: ', error)
 
-
-
-  onMounted(() => {
-    fetchImages()
-  })
-
+  // })
 </script>
 
 <style lang="scss" scoped></style>
